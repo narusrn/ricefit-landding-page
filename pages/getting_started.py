a764870bbf9b6f8c37a5f9a9ca2534f63724cb5d
Narusorn Rojrattanatrai <41671821+narusrn@users.noreply.github.com>
2025-12-04 15:22:10 +0700

    Update getting_started.py

--- pages/getting_started.py
+++ pages/getting_started.py
@@ -9,25 +9,40 @@ st.sidebar.page_link('pages/register.py', label='Register')
 
 st.title("เริ่มต้นการใช้งาน Ricefit API")
 
+st.markdown("""
+ในการใช้งานข้อมูล ... ผ่านระบบ API จะต้องทำการสมัครใช้งานก่อน และส่งข้อมูลยืนยันตัวตนทุกครั้งที่เรียกการใช้งาน API
+
+ระบบยืนยันตัวตนของ API ใช้ตามมาตรฐาน OAuth 2.0 โดยรองรับวิธีการขอ Access Token ผ่านทางเว็บไซต์บริการข้อมูล RiceFit API เท่านั้น ซึ่งนักพัฒนาต้องทำตามขั้นตอนการสมัครและใช้งาน ดังนี้
+""")
+
+st.title("การสมัครการใช้งาน")
 
 st.markdown("""
-เพื่อใช้งาน API จำเป็นต้องสมัครสมาชิกผ่านเว็บไซต์ของเรา  
 
-- เริ่มต้นการใช้งาน: https://ricefit-landding-page-wcbsvzpxrxwy5atzdeuxdf.streamlit.app/getting_started
-- สมัครสมาชิก: https://ricefit-landding-page-wcbsvzpxrxwy5atzdeuxdf.streamlit.app/register
+ผู้ใช้งานทั่วไปและนักพัฒนาสามารถสมัครใช้บริการข้อมูล Ricefit API ได้ที่เว็บไซต์ https://ricefit-landding-page-wcbsvzpxrxwy5atzdeuxdf.streamlit.app/register
+
+เมื่อเข้าสู่เว็บไซต์แล้ว กรอกข้อมูลส่วนตัวเพื่อใช้ในการยืนยันตัวตน และวัตถุประสงค์ในสมัครเข้าใช้งาน
+
+""")
+
+
+st.markdown("""
 
 หลังจากสมัครสมาชิกเรียบร้อยแล้ว ระบบจะส่ง **OAuth Access Token** ให้ทางอีเมลของคุณ  
 โปรดตรวจสอบอีเมลและเก็บ Token ไว้ให้ดี เพราะเป็นรหัสสำหรับเข้าถึง API ของคุณ  
 
-### ตัวอย่างการเรียก API
-```bash
-curl -X GET \
-'https://www.nectec.or.th/innovation/innovation-service/digital-agri-api' \
--H 'accept: application/json' \
--H 'authorization: Bearer <ACCESS_TOKEN>'
-```
 
 """)
+
+# ### ตัวอย่างการเรียก API
+# ```bash
+# curl -X GET \
+# 'https://www.nectec.or.th/innovation/innovation-service/digital-agri-api' \
+# -H 'accept: application/json' \
+# -H 'authorization: Bearer <ACCESS_TOKEN>'
+# ```
+
+
 
 
 
