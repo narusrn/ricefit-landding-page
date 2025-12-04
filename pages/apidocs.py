eb383afcea21e338467a21d9267aceac4c66ae12
Narusorn Rojrattanatrai <41671821+narusrn@users.noreply.github.com>
2025-12-04 19:59:26 +0700

    Update apidocs.py

--- pages/apidocs.py
+++ pages/apidocs.py
@@ -1,14 +1,21 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 # st.sidebar.page_link('app.py', label='Home')
 st.sidebar.page_link('pages/getting_started.py', label='Getting Started')
 st.sidebar.page_link('pages/register.py', label='Register')
 st.sidebar.page_link('pages/apidocs.py', label='API ที่ให้บริการ')
 
-st.markdown("""
-<div style="text-align:center; margin-top: 120px;">
-    <h2>API ที่ให้บริการ</h2>
-    <p style="font-size: 18px; color: gray;">⚙️ หน้านี้กำลังปรับปรุง...</p>
-    <p>โปรดกลับมาอีกครั้งเร็ว ๆ นี้</p>
-</div>
-""", unsafe_allow_html=True)
+# st.markdown("""
+# <div style="text-align:center; margin-top: 120px;">
+#     <h2>API ที่ให้บริการ</h2>
+#     <p style="font-size: 18px; color: gray;">⚙️ หน้านี้กำลังปรับปรุง...</p>
+#     <p>โปรดกลับมาอีกครั้งเร็ว ๆ นี้</p>
+# </div>
+# """, unsafe_allow_html=True)
+
+components.iframe(
+    "https://www.nectec.or.th/innovation/innovation-service/digital-agri-api/docs#/",
+    height=600,
+    scrolling=True
+)
