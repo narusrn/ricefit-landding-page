0ed0b691591b9d21f88d328685c90a76db657865
Narusorn Rojrattanatrai <41671821+narusrn@users.noreply.github.com>
2025-12-04 16:44:54 +0700

    Update apidocs.py

--- pages/apidocs.py
+++ pages/apidocs.py
@@ -4,3 +4,7 @@ import streamlit as st
 st.sidebar.page_link('pages/getting_started.py', label='Getting Started')
 st.sidebar.page_link('pages/register.py', label='Register')
 st.sidebar.page_link('pages/apidocs.py', label='API ที่ให้บริการ')
+
+st.title("API ที่ให้บริการ")
+st.info("⚙️ หน้านี้กำลังปรับปรุง...")
+st.write("โปรดกลับมาอีกครั้งเร็ว ๆ นี้")
