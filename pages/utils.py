e863c1f1b449a1bd7ef9c6ed4c26bd5735d50ced
Narusorn Rojrattanatrai <41671821+narusrn@users.noreply.github.com>
2026-01-11 17:55:36 +0700

    Delete pages/utils.py

--- pages/utils.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-def sidebar_options():
-  st.sidebar.page_link('pages/getting_started.py', label='Getting Started')
-  st.sidebar.page_link('pages/register.py', label='Register')
-  st.sidebar.page_link('pages/apidocs.py', label='API ที่ให้บริการ')
-  st.sidebar.page_link(
-      'pages/rice_phenotype_api_docs.py',
-      label='API ลักษณะพันธุ์ข้าว (Rice Phenotype)'
-  )
